2b0c4bc16a9cb086e22e078e137d3f4c307f8cdc
Andrew Persic <persic.andrew@gmail.com>
2024-02-11 11:54:16 +0000

    setup tee

--- setup.sh
+++ setup.sh
@@ -2,9 +2,9 @@ cd /root
 git clone https://github.com/persuck/memory-pixel-interp.git
 cd memory-pixel-interp
 mkdir /logs
-conda install -y -c conda-forge x264=='1!164.3095' ffmpeg=6.1.1 > /logs/setup.log
-echo "\nDone installing H264. Installing python packages...\n" >> /logs/setup.log
-pip install -r requirements.txt >> /logs/setup.log
+conda install -y -c conda-forge x264=='1!164.3095' ffmpeg=6.1.1 | tee /logs/setup.log
+echo "\nDone installing H264. Installing python packages...\n" | tee -a /logs/setup.log
+pip install -r requirements.txt | tee -a /logs/setup.log
 touch ~/.no_auto_tmux
 conda init bash
 exit 0
